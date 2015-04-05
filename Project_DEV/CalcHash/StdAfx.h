d9bdd407be50e5f0e31af66806c144da9883b7df
Mikhail Maslov <mkmaslov@gmail.com>
2015-04-05 20:31:19 +0300

    Hash Calculator Header File
    Header file, used by our code.

---
+++ Project_DEV/CalcHash/StdAfx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+//Some stuff that some IDE's require.
+
+//#if !defined(AFX_STDAFX_H__B0F35FA2_C919_4860_8006_B2E1BB941C53__INCLUDED_)
+//#define AFX_STDAFX_H__B0F35FA2_C919_4860_8006_B2E1BB941C53__INCLUDED_
+
+//#if _MSC_VER > 1000
+//#pragma once
+//#endif // _MSC_VER > 1000
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+//#endif // !defined(AFX_STDAFX_H__B0F35FA2_C919_4860_8006_B2E1BB941C53__INCLUDED_)
